f103686bb53e3a77374c34407e6f107686468de5
franklin <igahfranklyn@gmail.com>
2024-09-03 16:57:09 +0100

    dark mode toggler

--- app/components/mode-toggle.tsx
+++ app/components/mode-toggle.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import { Button } from "./ui/button";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
-import { MoonStar, LampCeiling } from 'lucide-react';
+import { SunIcon } from "@radix-ui/react-icons";
+import { LampCeiling, MoonStarIcon } from 'lucide-react';
 
 import { useTheme } from "next-themes";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
 
   return (
     <Button
@@ -15,10 +19,13 @@ export function ModeToggle() {
       type="button"
       size="icon"
       className="px-2 fixed top-10 right-10 z-10"
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      onClick={toggleTheme}
     >
-      <SunIcon className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
-      <LampCeiling className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
+      {theme === "dark" ? (
+        <LampCeiling className="h-[1.2rem] w-[1.2rem] text-[#000]"  size={40}/>
+      ) : (
+        <MoonStarIcon className="h-[1.2rem] w-[1.2rem] text-neutral-800" size={40} />
+      )}
     </Button>
   );
 }
