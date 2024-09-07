52e4b073eb6b9ee94a7ddaa6938e455a0e68b9f8
franklin <igahfranklyn@gmail.com>
2024-09-07 10:57:06 +0100

    highlight hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { FloatingDockIcons } from "./components/icons/FloatingDockIcons";
 import { MacbookScrollHero } from "./components/hero/MacbookScrollHero";
+import { HighlightHero } from "./components/hero/HighlightHero";
 
 export default function Home() {
   return (
     <main className="h-screen">
       <FloatingDockIcons />
-      <MacbookScrollHero />
+      {/* <MacbookScrollHero /> */}
+      <HighlightHero />
     </main>
   );
 }
